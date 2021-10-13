e48ce2c430709d89aca3f269b3961120ef3566ec
YaneriPerdomo <92409304+YaneriPerdomo@users.noreply.github.com>
2021-10-12 21:18:06 -0400

    Add files via upload

---
+++ Área.cpp
@@ -0,0 +1,26 @@
+/*"construye un programa que al recibir como dato el radio de un circulo
+calcule e imprima tanto su área como la longitud de su curcunferencia
+
+El área de un circulo la calculamos como:
+Área
+
+La circunferencia del circulo La calculamos de la siguiente forma:
+Longitud de la circunferencia  2 * pi * radio*/
+
+#include <iostream>
+#include <math>.h>
+using namespace std;
+int main(void)}
+
+    double radio, area, longitud;
+
+    cout <<Ingrese el radio del circulo:  " << endl;"
+    cin  >>radio;
+
+    área  =  M_PI  * pow(radio,2);
+    Longitud  = 2 * radio;
+
+    cout << "El area del circulo es: " << area << endl;
+    cout  << "La longitud de la circunferencia es: " << longitud  << endl;
+
+}
